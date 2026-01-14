05340a54b4191e87116a136c3f2f861d322ce4ce
Fernando Dias <mrfernandodias@gmail.com>
2026-01-14 16:15:57 -0300

    implement database schemas

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+config();
+
+export default defineConfig({
+  schema: './src/db/schema/index.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  verbose: true,
+  strict: true,
+});
---
+++ src/db/schema/index.ts
@@ -0,0 +1,3 @@
+// Export all schemas
+export * from './app';
+// export * from './auth';
